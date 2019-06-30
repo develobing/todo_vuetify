298927bca587603144dd7f13252576a2a087ec26
Robin <philoin88@gmail.com>
2019-07-01 03:01:34 +0900

    Navigation Drawer에 메뉴 추가

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -16,7 +16,18 @@
     </v-toolbar>
 
     <v-navigation-drawer app class="primary" v-model="drawer">
-      <p class="info">test</p>
+      <v-list>
+        <v-list-tile v-for="link in links" :key="link.text" router :to="link.route">
+          <v-list-tile-action>
+            <v-icon class="white--text">{{ link.icon }}</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title class="white--text">
+              {{ link.text }}
+            </v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+      </v-list>
     </v-navigation-drawer>
   </nav>
 </template>
@@ -25,7 +36,12 @@
 export default {
   name: "Navbar",
   data: () => ({
-    drawer: false
+    drawer: false,
+    links: [
+      { icon: 'dashboard', text: 'Dashboard', route: '/' },
+      { icon: 'folder', text: 'My Projects', route: '/projects' },
+      { icon: 'person', text: 'Team', route: '/team' },
+    ]
   })
 }
 </script>
