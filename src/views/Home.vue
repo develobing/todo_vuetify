0877669c9c0a340181828f24451b5cb25c2692bb
Robin <philoin88@gmail.com>
2019-06-30 23:02:47 +0900

    Default 프로젝트 구조 제거 및 초기 작업 준비

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,11 @@
 <template>
-  <HelloWorld />
+  <div class="home">
+    <h1>Home Page</h1>
+  </div>
 </template>
 
 <script>
-  import HelloWorld from '../components/HelloWorld'
+export default {
 
-  export default {
-    components: {
-      HelloWorld
-    }
-  }
+}
 </script>
