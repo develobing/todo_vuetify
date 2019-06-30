0e0a55a48e639a815326d1b59e2621ac0af14156
Robin <philoin88@gmail.com>
2019-07-01 02:39:34 +0900

    Navigation Drawer 추가

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,6 +1,7 @@
 <template>
   <nav>
     <v-toolbar flat app>
+      <v-toolbar-side-icon class="grey--text" @click="drawer = !drawer"></v-toolbar-side-icon>
       <v-toolbar-title class="text-uppercase grey--text">
         <span class="font-weight-light">Todo</span>
         <span>Robin</span>
@@ -13,15 +14,22 @@
         <v-icon right>exit_to_app</v-icon>
       </v-btn>
     </v-toolbar>
+
+    <v-navigation-drawer app class="indigo" v-model="drawer">
+      <p>test</p>
+    </v-navigation-drawer>
   </nav>
 </template>
 
 <script>
 export default {
-  name: "Navbar"
+  name: "Navbar",
+  data: () => ({
+    drawer: false
+  })
 }
 </script>
 
 <style scoped>
-</style>
 
+</style>
