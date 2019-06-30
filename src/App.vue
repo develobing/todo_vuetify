f6bb0680f4f6a8fbe68bedb8f5e7c0addb403fff
Robin <philoin88@gmail.com>
2019-07-01 02:30:52 +0900

    상단 toolbar 추가

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
-  <v-app>
+  <v-app class="grey lighten-4">
+    <Navbar/>
     <v-content>
       <router-view/>
     </v-content>
@@ -7,8 +8,11 @@
 </template>
 
 <script>
+import Navbar from "@/components/Navbar";
+
 export default {
   name: 'App',
+  components: { Navbar },
   data () {
     return {
 
---
+++ src/components/Navbar.vue
@@ -0,0 +1,27 @@
+<template>
+  <nav>
+    <v-toolbar flat app>
+      <v-toolbar-title class="text-uppercase grey--text">
+        <span class="font-weight-light">Todo</span>
+        <span>Robin</span>
+      </v-toolbar-title>
+
+      <v-spacer></v-spacer>
+
+      <v-btn flat color="grey">
+        <span>Sign Out</span>
+        <v-icon right>exit_to_app</v-icon>
+      </v-btn>
+    </v-toolbar>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: "Navbar"
+}
+</script>
+
+<style scoped>
+</style>
+
