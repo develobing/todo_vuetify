8d016fdbc523741d0b4cb6b9616359a0f2d22707
Robin <philoin88@gmail.com>
2019-07-01 23:16:43 +0900

    프로젝트 추가 하는 Popup 추가

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -40,7 +40,12 @@
           </v-avatar>
           <p class="white--text subheading mt-1 text-xs-center text-lg-center">Robin</p>
         </v-flex>
+
+        <v-flex class="mt-4 mb-3">
+          <Popup/>
+        </v-flex>
       </v-layout>
+
       <v-list>
         <v-list-tile v-for="link in links" :key="link.text" router :to="link.route">
           <v-list-tile-action>
@@ -58,8 +63,11 @@
 </template>
 
 <script>
+import Popup from './Popup'
+
 export default {
   name: "Navbar",
+  components: { Popup },
   data: () => ({
     drawer: false,
     links: [
---
+++ src/components/Popup.vue
@@ -0,0 +1,48 @@
+<template>
+  <v-dialog max-width="600px">
+    <v-btn flat slot="activator" class="success">Add new project</v-btn>
+    <v-card>
+      <v-card-title>
+        <h2>Add a New Project</h2>
+      </v-card-title>
+
+      <v-card-text>
+        <v-form class="px-3">
+          <v-text-field label="Title" v-model="title" prepend-icon="folder"></v-text-field>
+          <v-textarea label="Information" v-model="content" prepend-icon="edit"></v-textarea>
+
+          <v-menu>
+            <v-text-field :value="formattedDate" slot="activator" label="Due Date" prepend-icon="date_range"></v-text-field>
+            <v-date-picker v-model="due"></v-date-picker>
+          </v-menu>
+          <v-spacer></v-spacer>
+
+          <v-btn flat class="success mx-0 mt-3" @click="submit">Add Project</v-btn>
+        </v-form>
+      </v-card-text>
+    </v-card>
+  </v-dialog>
+</template>
+
+<script>
+import format from 'date-fns/format'
+
+export default {
+  name: 'Popup',
+  data: () => ({
+    title: '',
+    content: '',
+    due: null
+  }),
+  computed: {
+    formattedDate() {
+      return this.due ? format(this.due, 'Do MMM YYYY') : ''
+    }
+  },
+  methods: {
+    submit() {
+      console.log(this.title, this.content, this.due)
+    }
+  }
+}
+</script>
