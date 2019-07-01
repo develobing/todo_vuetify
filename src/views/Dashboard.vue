92b5f1fb63732b23faa6bad22e7d1ebadc00fb0a
Robin <philoin88@gmail.com>
2019-07-01 22:14:57 +0900

    Dashboard에 정렬 기능 추가

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -3,6 +3,17 @@
     <h1 class="subheading grey--text">Dashboard</h1>
 
     <v-container class="my-5">
+      <v-layout row class="mb-3">
+        <v-btn small flat color="grey" @click="sortBy('title')">
+          <v-icon left>folder</v-icon>
+          <span class="caption text-lowercase">By project name</span>
+        </v-btn>
+        <v-btn small flat color="grey" @click="sortBy('person')">
+          <v-icon left>person</v-icon>
+          <span class="caption text-lowercase">By person</span>
+        </v-btn>
+      </v-layout>
+
       <v-card flat class="pa-1" v-for="project in projects" :key="project.title">
         <v-layout row wrap :class="`pa-3 project ${project.status}`">
           <v-flex xs12 md6>
@@ -18,8 +29,9 @@
             <div>{{ project.due }}</div>
           </v-flex>
           <v-flex xs6 sm4 md2>
-            <div class="a grey--text">Status</div>
-            <div>{{ project.status }}</div>
+            <div class="right">
+              <v-chip small :class="`${project.status} white--text caption my-2`">{{ project.status }}</v-chip>
+            </div>
           </v-flex>
         </v-layout>
         <v-divider></v-divider>
@@ -38,8 +50,12 @@ export default {
       { title: 'Design video thumbnails', person: 'Kevin', due: '20th Jun 2018', status: 'complete', content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt consequuntur eos eligendi illum minima adipisci deleniti, dicta mollitia enim explicabo fugiat quidem ducimus praesentium voluptates porro molestias non sequi animi!'},
       { title: 'Create a community forum', person: 'David', due: '20th Jun 2018', status: 'overdue', content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt consequuntur eos eligendi illum minima adipisci deleniti, dicta mollitia enim explicabo fugiat quidem ducimus praesentium voluptates porro molestias non sequi animi!'},
     ]
-  })
-
+  }),
+  methods: {
+    sortBy(prop) {
+      this.projects.sort((a, b) => a[prop] < b[prop] ? -1 : 1)
+    }
+  }
 }
 </script>
 
@@ -52,5 +68,14 @@ export default {
 }
 .project.overdue {
   border-left: 4px solid tomato;
+}
+.v-chip.complete {
+  background-color: lightseagreen;
+}
+.v-chip.ongoing {
+  background-color: orange;
+}
+.v-chip.overdue {
+  background-color: tomato;
 }
 </style>
